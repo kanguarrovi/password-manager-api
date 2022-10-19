07ebe35c9a16e1a33a4e06ab71a19e66a2000731
Carlos Arroyo Villalobos <carlosarrovi96@gmail.com>
2022-10-19 00:35:34 -0600

    Add complexity parameter into password generator

--- utils/password_generator.py
+++ utils/password_generator.py
@@ -1,14 +1,25 @@
 import random
 import string
 
-def password_generator(length: int):
+def password_generator(length: int, complexity=3):
 
-    lower = string.ascii_lowercase
-    upper = string.ascii_uppercase
-    num = string.digits
-    symbols = string.punctuation
+    def characters_generator(length:int, complexity: int):
 
-    characters = lower + upper + num + symbols
+        lower = string.ascii_lowercase
+        upper = string.ascii_uppercase
+        numbers = string.digits
+        symbols = string.punctuation
+
+        if complexity == 1:
+            return lower
+        elif complexity == 2:
+            return lower + upper
+        elif complexity == 3:
+            return lower + upper + numbers
+        elif complexity == 4:
+            return lower + upper + numbers + symbols
+
+    characters = characters_generator(length, complexity)
 
     character_random_list = random.sample(characters, length)
 
